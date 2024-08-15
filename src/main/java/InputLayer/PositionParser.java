5eb4b4fb4ee34cac9ec0651ec3082426f18ae009
Rachel Jaisey <jaiseyrachel@gmail.com>
2024-08-15 10:58:43 +0100

    Feat: Create PositionParser class with signature for parse method

---
+++ src/main/java/InputLayer/PositionParser.java
@@ -0,0 +1,10 @@
+package InputLayer;
+
+public class PositionParser {
+    // takes a string input
+    // returns a position object
+
+    public Position parse(String input){
+        return null;
+    }
+}
