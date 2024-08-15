4ae3999bc4d6a9adbcf6d0f4d165cbe132e97c91
Rachel Jaisey <jaiseyrachel@gmail.com>
2024-08-15 10:53:19 +0100

    Feat: Create InstructionParser class with a parse method

---
+++ src/main/java/InputLayer/InstructionParser.java
@@ -0,0 +1,12 @@
+package InputLayer;
+
+import java.util.ArrayList;
+
+public class InstructionParser {
+    // takes a string input
+    // returns an array list of instruction enums
+
+    public ArrayList<Instruction> parse(String input){
+        return null;
+    }
+}
