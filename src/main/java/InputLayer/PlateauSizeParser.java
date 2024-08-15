c80cdafd6c82c32c6755fb853e14eb704bdc28d4
Rachel Jaisey <jaiseyrachel@gmail.com>
2024-08-15 10:56:32 +0100

    Feat: Create PlateauSizeParser class with signature for parse method

---
+++ src/main/java/InputLayer/PlateauSizeParser.java
@@ -0,0 +1,10 @@
+package InputLayer;
+
+public class PlateauSizeParser {
+    // takes a string
+    // returns a PlateauSize object
+
+    public PlateauSize parse(String input){
+        return null;
+    }
+}
