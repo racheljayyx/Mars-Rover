a52be28118af4e2a2dd81a7233d40c64fb0d2596
Rachel Jaisey <jaiseyrachel@gmail.com>
2024-08-15 14:57:45 +0100

    Test: Create method singature for all test cases

--- src/test/java/InputLayer/PlateauSizeParserTest.java
+++ src/test/java/InputLayer/PlateauSizeParserTest.java
@@ -1,8 +1,28 @@
 package InputLayer;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlateauSizeParserTest {
 
+    @Test
+    void parseTest_ValidInput() {
+
+    }
+
+    @Test
+    void parseTest_NegativeInput() {
+
+    }
+
+    @Test
+    void parseTest_InvalidInput() {
+
+    }
+
+    void parseTest_NullInput() {
+
+    }
 
 }
