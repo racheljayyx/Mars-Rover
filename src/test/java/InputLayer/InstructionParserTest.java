a77346111f2432222019e2e71d7acb87f7f82b28
Rachel Jaisey <jaiseyrachel@gmail.com>
2024-08-15 11:44:33 +0100

    Test: Create InstructionParserTest class containing six test methods

---
+++ src/test/java/InputLayer/InstructionParserTest.java
@@ -0,0 +1,37 @@
+package InputLayer;
+
+import org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+class InstructionParserTest {
+
+    @Test
+    void parseTest_SingleInstruction() {
+    }
+
+    @Test
+    void parseTest_MultipleInstructions() {
+
+    }
+
+    @Test
+    void parseTest_InvalidInstructions() {
+
+    }
+
+    @Test
+    void parseTest_EmptyString() {
+
+    }
+
+    @Test
+    void parseTest_NullInput() {
+
+    }
+
+    @Test
+    void parseTest_WithWhiteSpaces() {
+
+    }
+
+}
