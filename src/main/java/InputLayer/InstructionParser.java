ea9af26674e2666a55debe45e83c4fe252e76d8c
Rachel Jaisey <jaiseyrachel@gmail.com>
2024-08-15 14:26:29 +0100

    Feat: Implement logic to InstructionParser method

--- src/main/java/InputLayer/InstructionParser.java
+++ src/main/java/InputLayer/InstructionParser.java
@@ -1,6 +1,8 @@
 package InputLayer;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class InstructionParser {
     // takes a string input
@@ -8,6 +10,31 @@ public class InstructionParser {
 
     public static ArrayList<Instruction> parse(String input){
 
-        return null;
+        ArrayList<Instruction> instructions = new ArrayList<>();
+        if (input == null) return null;
+
+        Pattern pattern = Pattern.compile("^[lmrLMR]+$");
+        Matcher matcher = pattern.matcher(input);
+
+        if (!matcher.find()){
+            throw new IllegalArgumentException("Invalid input.");
+        }
+
+        for (char ch : input.toUpperCase().toCharArray()){
+            switch (ch) {
+                case 'L':
+                    instructions.add(Instruction.L);
+                    break;
+                case 'R':
+                    instructions.add(Instruction.R);
+                    break;
+                case 'M':
+                    instructions.add(Instruction.M);
+                    break;
+                    
+            }
+        }
+
+        return instructions;
     }
 }
