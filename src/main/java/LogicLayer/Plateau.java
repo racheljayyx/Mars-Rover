4492b14d04a120933ece8c8a69c93a9bd1193f29
Rachel Jaisey <jaiseyrachel@gmail.com>
2024-08-16 14:03:50 +0100

    Feat: Add functionality to get and set PlateauSize and convert Plateau class to a Singleton

--- src/main/java/LogicLayer/Plateau.java
+++ src/main/java/LogicLayer/Plateau.java
@@ -1,4 +1,26 @@
 package LogicLayer;
 
+import InputLayer.PlateauSize;
+import InputLayer.PlateauSizeParser;
+
 public class Plateau {
+    private PlateauSize plateauSize;
+
+    private static final Plateau instance = new Plateau();
+
+    private Plateau() {
+    };
+
+    public static Plateau getInstance() {
+        return instance;
+    }
+
+    public void setPlateauSize(String input){
+        plateauSize = PlateauSizeParser.parse(input);
+    }
+
+
+    public PlateauSize getPlateauSize() {
+        return plateauSize;
+    }
 }
