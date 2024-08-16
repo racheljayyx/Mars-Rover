bd8d857563d90f6de3109b22a4e13bfcd4761573
Rachel Jaisey <jaiseyrachel@gmail.com>
2024-08-16 09:02:06 +0100

    Feat: Add logic to Rover class, constructor and necessary getters and setters

--- src/main/java/LogicLayer/Rover.java
+++ src/main/java/LogicLayer/Rover.java
@@ -1,4 +1,25 @@
 package LogicLayer;
 
+import InputLayer.*;
+
 public class Rover {
+    private int id = 0;
+    private Position roverPosition;
+
+    public Rover(Position roverPosition) {
+        this.roverPosition = roverPosition;
+        this.id +=1;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Position getRoverPosition() {
+        return roverPosition;
+    }
+
+    public void setRoverPosition(Position roverPosition) {
+        this.roverPosition = roverPosition;
+    }
 }
